ee456f4302111498f883368ec06c2e9f8f9c4731
Alex Bellamy <alexlbellamy@gmail.com>
2020-06-05 11:04:18 +0200

    add additional tests and functionality - greetings header and button

--- cypress/integration/userCanClickOnButtonForGreetingMessage.feature.js
+++ cypress/integration/userCanClickOnButtonForGreetingMessage.feature.js
@@ -1,5 +1,7 @@
 describe('user can click on button', () => {
     it('test', () => {
         cy.visit('http://localhost:3003')
+        cy.contains("Greetings")
+        cy.get("#quote").click()
     })
 })
